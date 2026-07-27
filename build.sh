575a120d79e7b01da69f4d82abfad5a4045e3d7a
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2026-07-27 23:25:03 +0300

    remove debug symbols for now?

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eux
 which ${CC:-} || CC=gcc
-# -O0 -fsanitize=address
+# -g -O0 -fsanitize=address
 rm -f gso.o
-$CC -c gso.c -I. -Wpedantic -Wall -Werror -g
+$CC -c gso.c -I. -Wpedantic -Wall -Werror
